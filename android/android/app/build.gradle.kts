1790f913542d2c5f165630f076522e7298f5fa30
serbberuz619 <lilsainth2455@gmail.com>
2026-03-11 22:15:57 +0800

    update na yan

--- android/android/app/build.gradle.kts
+++ android/android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.android"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "com.iselco.mobile"
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -21,11 +21,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.android"
+        applicationId = "com.iselco.mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
---
+++ android/android/app/src/main/kotlin/com/iselco/mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.iselco.mobile
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity()
